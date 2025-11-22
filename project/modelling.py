a7b8aed7bdc870c9a05868be5d486a249f1c01c2
ayalya <alyafauziaz20@gmail.com>
2025-11-23 01:42:48 +0700

    handling error parameters

--- project/modelling.py
+++ project/modelling.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     input_example = X_train[0:5]
     n_neighbors = int(sys.argv[1]) if len(sys.argv) > 1 else 50
-    leaf_size   = int(sys.argv[2]) if len(sys.argv) > 2 else 30
+    leaf_size   = int(sys.argv[2]) if len(sys.argv) > 2 else 35
 
     with mlflow.start_run():
         model = KNeighborsClassifier(n_neighbors=n_neighbors,
