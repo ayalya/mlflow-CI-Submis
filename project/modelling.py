3ed3fc2cc774fb1c22ce587f3c55d5e8dab38b33
ayalya <alyafauziaz20@gmail.com>
2025-11-23 02:31:23 +0700

    Handling n_neighbors again

--- project/modelling.py
+++ project/modelling.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
 
     input_example = X_train[0:5]
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_neighbors", type=int, default=5)
-    parser.add_argument("--leaf_size", type=int, default=35)
+    parser.add_argument("--n_neighbors", type=int, default=50)
+    parser.add_argument("--leaf_size", type=int, default=100)
     args = parser.parse_args()
 
     n_neighbors = args.n_neighbors
